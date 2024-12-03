657b501abf90a605c433c539ba5224e575478218
Gabriel Legault <gabriel.legault@goto.com>
2024-12-03 13:33:57 -0500

    part 2

--- day03-java/solution.java
+++ day03-java/solution.java
@@ -8,6 +8,9 @@ public class solution {
 
         test(sol1("test-input.txt"), 161);
         test(sol1("input.txt"), 173529487);
+
+        test(sol2("test-input-2.txt"), 48);
+        test(sol2("input.txt"), 99532691);
     }
 
     private static String readFile(String path) {
@@ -21,7 +24,6 @@ public class solution {
 
     private static void test(int actual, int expected) {
         if (actual != expected) {
-
             System.out.println(String.format("❌ Test fail, Expected: %s, Actual: %s", expected, actual));
 
         } else {
@@ -49,6 +51,59 @@ public class solution {
         return sum;
     }
 
+    private static int sol2(String fileName) {
+        var input = readFile(fileName);
+
+        var regex = "(don't\\(\\)|do\\(\\)|mul\\((\\d+),(\\d+)\\))";
+
+        var result = (Result) Pattern.compile(regex)
+                .matcher(input)
+                .results()
+                .map(r -> {
+                    if (r.group(0).startsWith("mul(")) {
+                        var x = Integer.parseInt(r.group(2));
+                        var y = Integer.parseInt(r.group(3));
+                        return new Mul(x, y);
+                    }
+                    if (r.group(0).startsWith("do()")) {
+                        return new Do();
+                    }
+                    if (r.group(0).startsWith("don't()")) {
+                        return new DoNot();
+                    }
+                    throw new Error("Something is not correct in the regex");
+                }).reduce(new Result(true, 0), (acc, element) -> {
+
+                    var _result = (Result) acc;
+
+                    var enabled = _result.enabled;
+                    var sum = _result.sum;
+                    if (enabled && element instanceof Mul) {
+                        var mul = (Mul) element;
+                        return new Result(enabled, _result.sum + (mul.x * mul.y));
+                    }
+                    if (element instanceof Do) {
+                        return new Result(true, sum);
+                    }
+                    if (element instanceof DoNot) {
+                        return new Result(false, sum);
+                    }
+
+                    return new Result(enabled, sum);
+                });
+
+        return result.sum;
+    }
+
     public record Mul(int x, int y) {
+    }
+
+    public record Do() {
+    }
+
+    public record DoNot() {
+    }
+
+    public record Result(boolean enabled, int sum) {
     }
 }
