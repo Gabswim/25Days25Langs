34668fd8efc79e717b3ac55eed5fdf5759e8406d
Gabriel Legault <gabriel.legault@goto.com>
2024-12-03 08:46:04 -0500

    clean up

--- day03-java/solution.java
+++ day03-java/solution.java
@@ -1,22 +1,14 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Scanner;
-import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class solution {
     public static void main(String[] args) {
 
-    
         test(sol1("test-input.txt"), 161);
-        test(sol1("input.txt"), 161);
+        test(sol1("input.txt"), 173529487);
     }
-
 
     private static String readFile(String path) {
         try {
@@ -27,24 +19,23 @@ public class solution {
         }
     }
 
-    
-
     private static void test(int actual, int expected) {
         if (actual != expected) {
 
             System.out.println(String.format("❌ Test fail, Expected: %s, Actual: %s", expected, actual));
-            
+
         } else {
             System.out.println("✅ Test pass");
-            
+
         }
     }
 
     private static int sol1(String fileName) {
         var input = readFile(fileName);
-        var sum = 0;
+
         var regex = "mul\\((\\d+),(\\d+)\\)";
-        var sections = Pattern.compile(regex)
+
+        var sum = Pattern.compile(regex)
                 .matcher(input)
                 .results()
                 .map(r -> {
@@ -52,13 +43,12 @@ public class solution {
                     var y = Integer.parseInt(r.group(2));
                     return new Mul(x, y);
                 })
-                .collect(Collectors.toList());
+                .mapToInt(mul -> mul.x * mul.y)
+                .reduce(0, Integer::sum);
 
-        for (Mul mul : sections) {
-            sum = sum + (mul.x * mul.y);
-        }
         return sum;
     }
 
-    public record Mul(int x, int y) {}
+    public record Mul(int x, int y) {
+    }
 }
