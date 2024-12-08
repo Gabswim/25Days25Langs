93d564e32405459159ac9f9f69bc3e89ad89b478
Gabriel Legault <gabriel.legault@goto.com>
2024-12-08 18:59:28 -0500

    feat: Day 04 - zig (#7)
    * WIP: read file and parse
    
    * part 1
    
    * part 2
    
    * Clean up vscode file
    
    * clean up

--- day04-zig/solution.zig
+++ day04-zig/solution.zig
@@ -1,5 +1,147 @@
 const std = @import("std");
 
+fn readFileAndParse(comptime path: []const u8, allocator: std.mem.Allocator) !std.ArrayList(std.ArrayList(u8)) {
+    const file = @embedFile(path);
+    var it = std.mem.tokenize(u8, file, "\n");
+    var grid = std.ArrayList(std.ArrayList(u8)).init(allocator);
+
+    while (it.next()) |line| {
+        var row = std.ArrayList(u8).init(allocator);
+        try row.appendSlice(line);
+        try grid.append(row);
+    }
+
+    return grid;
+}
+
+test "sol1" {
+    var sum: i32 = 0;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const grid = try readFileAndParse("input.txt", allocator);
+
+    defer {
+        for (grid.items) |row| {
+            row.deinit();
+        }
+        grid.deinit();
+    }
+
+    const row = grid.items.len;
+    const column = grid.items[0].items.len;
+
+    for (0..row) |i| {
+        for (0..column) |j| {
+
+            //std.debug.print("Sum: {d}\n", .{sum});
+            // Horizontal
+            if (j + 3 < column) {
+                const horizontal = grid.items[i].items[j .. j + 4];
+
+                std.debug.print("Horizontal: {s}\n", .{horizontal});
+                if (std.mem.eql(u8, horizontal, "XMAS") or std.mem.eql(u8, horizontal, "SAMX")) {
+                    std.debug.print("Found horizontal: {s}\n", .{horizontal});
+                    sum += 1;
+                }
+            }
+
+            // Vertical
+            if (i + 3 < row) {
+                const vertical = [_]u8{
+                    grid.items[i].items[j],
+                    grid.items[i + 1].items[j],
+                    grid.items[i + 2].items[j],
+                    grid.items[i + 3].items[j],
+                };
+
+                std.debug.print("Vertical: {s}\n", .{vertical});
+                if (std.mem.eql(u8, &vertical, "XMAS") or std.mem.eql(u8, &vertical, "SAMX")) {
+                    std.debug.print("Found vertical: {s}\n", .{vertical});
+                    sum += 1;
+                }
+            }
+
+            // Diagonal Left-to-Right
+            if (i + 3 < row and j + 3 < column) {
+                const diagonalLTR = [_]u8{
+                    grid.items[i].items[j],
+                    grid.items[i + 1].items[j + 1],
+                    grid.items[i + 2].items[j + 2],
+                    grid.items[i + 3].items[j + 3],
+                };
+
+                if (std.mem.eql(u8, &diagonalLTR, "XMAS") or std.mem.eql(u8, &diagonalLTR, "SAMX")) {
+                    std.debug.print("Found diagonal Left-to-Right: {s}\n", .{diagonalLTR});
+                    sum += 1;
+                }
+            }
+
+            // Diagonal Right-to-Left
+            const safeJ: i32 = @intCast(j);
+            if (i + 3 < row and safeJ - 3 >= 0) {
+                const diagonalRTL = [_]u8{
+                    grid.items[i].items[j],
+                    grid.items[i + 1].items[j - 1],
+                    grid.items[i + 2].items[j - 2],
+                    grid.items[i + 3].items[j - 3],
+                };
+
+                if (std.mem.eql(u8, &diagonalRTL, "XMAS") or std.mem.eql(u8, &diagonalRTL, "SAMX")) {
+                    std.debug.print("Found diagonal Right-to-Left: {s}\n", .{diagonalRTL});
+                    sum += 1;
+                }
+            }
+        }
+    }
+
+    std.debug.print("Sum: {d}\n", .{sum});
+
+    try std.testing.expectEqual(sum, 2406);
+}
+
+test "sol2" {
+    var sum: i32 = 0;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const grid = try readFileAndParse("input.txt", allocator);
+
+    defer {
+        for (grid.items) |row| {
+            row.deinit();
+        }
+        grid.deinit();
+    }
+
+    const row = grid.items.len;
+    const column = grid.items[0].items.len;
+
+    for (1..row - 1) |i| {
+        for (1..column - 1) |j| {
+            if (grid.items[i].items[j] == 'A') {
+                std.debug.print("Found A at {d}, {d}\n", .{ i, j });
+                const topLeft = grid.items[i - 1].items[j - 1];
+                const topRight = grid.items[i - 1].items[j + 1];
+                const bottomLeft = grid.items[i + 1].items[j - 1];
+                const bottomRight = grid.items[i + 1].items[j + 1];
+                const cond1 = (topLeft == 'M' and bottomRight == 'S') or (topLeft == 'S' and bottomRight == 'M');
+                const cond2 = (topRight == 'M' and bottomLeft == 'S') or (topRight == 'S' and bottomLeft == 'M');
+                if (cond1 and cond2) {
+                    std.debug.print("Found one cross at {d}, {d}\n", .{ i, j });
+                    sum += 1;
+                }
+            }
+        }
+    }
+
+    std.debug.print("Sum: {d}\n", .{sum});
+
+    try std.testing.expectEqual(sum, 1807);
+}
+
 pub fn main() void {
-    std.debug.print("Hello, World!\n", .{});
+    std.debug.print("Day 4!\n", .{});
 }
